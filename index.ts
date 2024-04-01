fe9bfe869ed4506b25145d88d279e3cb1e477b09
Pattisier <Principursa@protonmail.com>
2024-04-01 16:49:09 -0400

    added some stuff

--- index.ts
+++ index.ts
@@ -5,9 +5,14 @@ import * as express from "express"
 const app = express();
 const port = 3000;
 
-app.get('/', async (req, res) => {
+app.get('/',  (req, res) => {
     res.send("hello world")
 });
+app.get("/quote/:age",  (req,res) => { //anadir mas parametros aqui 
+    const age = req.params.age //un ejemplo para mostrar como se puede acceder los parametros
+    //calcula aqui 
+
+}) 
 
 app.listen(port, () => {
     console.log(`pensionfi app listening on port ${port}`);
